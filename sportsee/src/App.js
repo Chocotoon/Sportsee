d7e097f421bc10a6e8e6a08ab8c6a3b994434155
nicok <khamsophan@yahoo.fr>
2024-02-20 15:48:28 +0100

    set up sportsee

---
+++ sportsee/src/App.js
@@ -0,0 +1,14 @@
+import Header from '../src/components/Header'
+import VerticalNav from '../src/components/Vertical_nav'
+
+function App() {
+  return (
+    <div>
+    <Header/>
+    <VerticalNav/>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ sportsee/src/components/Vertical_nav.jsx
@@ -0,0 +1,11 @@
+import '../styles/Vertical_nav.css'
+
+function VerticalNav() {
+    return (
+        <div className='vertical_nav'>
+            
+        </div>
+    )
+}
+
+export default VerticalNav
